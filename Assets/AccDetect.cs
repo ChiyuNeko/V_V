20492ccc6b7d2d4699244be1b569091059990aa9
ChiyuNeko <joe26121@gmail.com>
2026-04-30 21:34:21 +0800

    UI update

--- Assets/AccDetect.cs
+++ Assets/AccDetect.cs
@@ -2,22 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AccDetect : MonoBehaviour
 {
     public Vector3 dir;
     public AudioSource clapAudio;
+    public Slider slider;
     public float threashold;
     public TextMeshProUGUI txt;
+    bool canPlay = true;
+    float maxThreashold = 0;
     // Start is called before the first frame update
     
     void Update()
     {
         dir = Input.acceleration;
-        txt.text = dir.ToString();
-        if(dir.x >= threashold)
+        if (dir.magnitude > maxThreashold)
+        {
+            txt.text = dir.magnitude.ToString();
+            maxThreashold = dir.magnitude;
+        }
+        threashold = 5 / (slider.value + 1);
+
+        if(dir.magnitude >= threashold && canPlay)
         {
             clapAudio.Play();
+            canPlay = false;
+        }
+        if (dir.magnitude < threashold)
+        {
+            canPlay = true;
         }
     }
 }
